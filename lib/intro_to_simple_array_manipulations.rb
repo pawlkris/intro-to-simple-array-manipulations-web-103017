c4c1466fbee8a842373489e903200cd3febbba49
Paul Kristapovich <paulkristapovich@gmail.com>
2017-10-19 19:06:57 +0000

    automatically backed up by learn

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,39 @@
+def using_push(array, item)
+  return array.push(item)
+end
+
+def using_unshift(array, item)
+  return array.unshift(item)
+end
+
+def using_pop(array)
+  return array.pop
+end
+
+def pop_with_args(array)
+  return arry.pop(2)
+end
+
+def using_shift(array)
+  return array.shift
+end
+
+def shift_with_args(array)
+  return array.shift(2)
+end
+
+def using_concat(array1, array2)
+  return array1.concat(array2)
+end
+
+def using_insert(array, item)
+  return array.insert(3,item)
+end
+
+def using_uniq(array)
+  return array.uniq
+end
+
+def using_flatten(array)
+  return array.flatten
+end
